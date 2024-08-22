03648e4ffaf24c5a1923da34393c7fbc6d4d6fd2
a-oyama <snow-sakura@outlook.jp>
2024-08-22 21:14:59 +0900

    componentへヘッダー
    ヘッダー設定をcomponentフォルダへ

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-import Header from "./Header";
+import Header from "./components/Header";
+import HomePage from "./components/HomePage";
+import BlogPage from "./components/BlogPage";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <HomePage />
+
+      <BlogPage />
     </div>
   );
 }
