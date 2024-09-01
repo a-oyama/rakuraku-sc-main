03a81865d78307352115039d10d768678c9530fe
a-oyama <snow-sakura@outlook.jp>
2024-09-01 22:24:10 +0900

    ブラウザ動作確認

---
+++ src/app/components/auth/profile.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useRouter } from "next/router"
+import { useState } from "react"
+import { useSupabase } from "../supabase-provider"
+import Loading from "@/app/loading"
+
+const Profile = () => {
+
+}
+
+
+
+
+
+
+
+
+
+export default Profile
